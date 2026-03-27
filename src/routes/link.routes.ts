2bfc9b460fec7741b18f956d11fdd3c32a3d9a65
agnesaz <137079947+agnesaz@users.noreply.github.com>
2026-03-27 20:00:27 +0100

    remove commented-out code

--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -1,16 +1,3 @@
-// import { Router } from 'express';
-// import { getLinks, createLink, updateLink, deleteLink } from '../controllers/link.controller';
-// import { protect } from '../middleware/auth.middleware';
-
-// const router = Router();
-
-// router.get('/', getLinks);          // public
-// router.post('/', protect, createLink);
-// router.put('/:id', protect, updateLink);
-// router.delete('/:id', protect, deleteLink);
-
-// export default router;
-
 import { Router } from 'express';
 import * as linkController from '../controllers/link.controller';
 import { protect } from '../middleware/auth.middleware';
